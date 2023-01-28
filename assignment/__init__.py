b6ced3128f863a7788f2d8807b1482cfdcff350d
Jacek <jacek.zgud2@gmail.com>
2023-01-28 22:19:23 +0100

    manual date input

--- assignment/__init__.py
+++ assignment/__init__.py
@@ -18,9 +18,20 @@ def years_merger(population, gdp_loc, emissions):
     pop_years = population.columns[4:]
     gdp_years = gdp_loc.columns[4:]
     em_years = emissions.Year.unique().astype('str')
-
     all_years = np.intersect1d(np.intersect1d(gdp_years, pop_years), em_years)
     emissions = emissions[emissions.Year.isin(all_years.astype('int'))]
     gdp_loc = pd.concat([gdp_loc.iloc[:, [0, 1, 2, 3]], gdp_loc.loc[:, all_years]], axis=1)
     population = pd.concat([population.iloc[:, [0, 1, 2, 3]], population.loc[:, all_years]], axis=1)
     return[population, gdp_loc, emissions]
+
+
+def years_interval_merger(population, gdp_loc, emissions, beginning, end):
+    beginning = int(beginning)
+    end = int(end) + 1
+    all_years = list(range(beginning, end))
+    emissions = emissions[emissions.Year.isin(all_years)]
+    all_years = list(map(str, all_years))
+    gdp_loc = pd.concat([gdp_loc.iloc[:, [0, 1, 2, 3]], gdp_loc.loc[:, all_years]], axis=1)
+    population = pd.concat([population.iloc[:, [0, 1, 2, 3]], population.loc[:, all_years]], axis=1)
+    return[population, gdp_loc, emissions]
+
--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import assignment as asn
 gdp = asn.file_opener('API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562')
 pop = asn.file_opener('API_SP.POP.TOTL_DS2_en_csv_v2_4751604')
 em = asn.file_opener('co2-fossil-by-nation_zip')
+
 pop = pop.dropna(axis=1, how='all')
 gdp = gdp.dropna(axis=1, how='all')
 em = em.dropna(axis=0, how='all')
 
-pop, gdp, em = asn.years_merger(pop, gdp, em)
+bg = input("Begining date:")
+end = input("End date:")
+if len(bg) != 0 and len(end) != 0:
+    pop, gdp, em = asn.years_interval_merger(pop, gdp, em, bg, end)
+else:
+    pop, gdp, em = asn.years_merger(pop, gdp, em)
+
+print(pop)
+# print(pop['Country Name'])
+
 
