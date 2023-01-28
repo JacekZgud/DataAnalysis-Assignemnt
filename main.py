47cedc1fcde6b0e385198b120186489545c63bbc
Jacek <jacek.zgud2@gmail.com>
2023-01-28 18:11:10 +0100

    test after pull

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import assignment as asn
 gdp = asn.file_opener('API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562')
 pop = asn.file_opener('API_SP.POP.TOTL_DS2_en_csv_v2_4751604')
 em = asn.file_opener('co2-fossil-by-nation_zip')
-
 pop = pop.dropna(axis=1, how='all')
 gdp = gdp.dropna(axis=1, how='all')
 em = em.dropna(axis=0, how='all')
