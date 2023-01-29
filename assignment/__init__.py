24a3d1923e2432bc327cf3e1a795a6c5cbb9541d
Jacek <jacek.zgud2@gmail.com>
2023-01-29 20:10:06 +0100

    finally merged data

--- assignment/__init__.py
+++ assignment/__init__.py
@@ -15,13 +15,13 @@ def file_opener(file_name):
 
 
 def years_merger(population, gdp_loc, emissions):
-    pop_years = population.columns[4:]
-    gdp_years = gdp_loc.columns[4:]
+    pop_years = population.columns[1:]
+    gdp_years = gdp_loc.columns[1:]
     em_years = emissions.Year.unique().astype('str')
     all_years = np.intersect1d(np.intersect1d(gdp_years, pop_years), em_years)
     emissions = emissions[emissions.Year.isin(all_years.astype('int'))]
-    gdp_loc = pd.concat([gdp_loc.iloc[:, [0, 1, 2, 3]], gdp_loc.loc[:, all_years]], axis=1)
-    population = pd.concat([population.iloc[:, [0, 1, 2, 3]], population.loc[:, all_years]], axis=1)
+    gdp_loc = pd.concat([gdp_loc.iloc[:, [0]], gdp_loc.loc[:, all_years]], axis=1)
+    population = pd.concat([population.iloc[:, [0]], population.loc[:, all_years]], axis=1)
     return[population, gdp_loc, emissions]
 
 
@@ -31,7 +31,6 @@ def years_interval_merger(population, gdp_loc, emissions, beginning, end):
     all_years = list(range(beginning, end))
     emissions = emissions[emissions.Year.isin(all_years)]
     all_years = list(map(str, all_years))
-    gdp_loc = pd.concat([gdp_loc.iloc[:, [0, 1, 2, 3]], gdp_loc.loc[:, all_years]], axis=1)
-    population = pd.concat([population.iloc[:, [0, 1, 2, 3]], population.loc[:, all_years]], axis=1)
+    gdp_loc = pd.concat([gdp_loc.iloc[:, [0]], gdp_loc.loc[:, all_years]], axis=1)
+    population = pd.concat([population.iloc[:, [0]], population.loc[:, all_years]], axis=1)
     return[population, gdp_loc, emissions]
-
--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import numpy as np
 import pandas as pd
 import assignment as asn
+import re
 
+# import data
 gdp = asn.file_opener('API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562')
 pop = asn.file_opener('API_SP.POP.TOTL_DS2_en_csv_v2_4751604')
 em = asn.file_opener('co2-fossil-by-nation_zip')
 
+# clean data
 pop = pop.dropna(axis=1, how='all')
 gdp = gdp.dropna(axis=1, how='all')
 em = em.dropna(axis=0, how='all')
+em = em[['Year', 'Country', 'Total', 'Per Capita']]
+pop = pop.drop(columns=['Country Code', 'Indicator Name', 'Indicator Code'])
+gdp = gdp.drop(columns=['Country Code', 'Indicator Name', 'Indicator Code'])
 
-bg = input("Begining date:")
-end = input("End date:")
+# Provide different year span:
+bg = input("Beginning year:")
+end = input("End year:")
 if len(bg) != 0 and len(end) != 0:
     pop, gdp, em = asn.years_interval_merger(pop, gdp, em, bg, end)
 else:
     pop, gdp, em = asn.years_merger(pop, gdp, em)
 
-print(pop)
-# print(pop['Country Name'])
+# Clean Country names
+pop['Country Name'] = pop['Country Name'].apply(lambda x: x.lower())
+gdp['Country Name'] = gdp['Country Name'].apply(lambda x: x.lower())
+em.Country = em.Country.apply(lambda x: x.lower())
+em.Country = em.Country.apply(lambda x: re.sub(" \(.*?\)", "", x))
+pop['Country Name'] = pop['Country Name'].apply(lambda x: re.sub(" \(.*?\)", "", x))
+gdp['Country Name'] = gdp['Country Name'].apply(lambda x: re.sub(" \(.*?\)", "", x))
 
+#  Adjust and merge gdp and pop
+pop = pop.melt(id_vars='Country Name', var_name='Year', value_name='Population')
+gdp = gdp.melt(id_vars='Country Name', var_name='Year', value_name='Gdp')
+pop_gdp = pd.merge(pop, gdp, how='inner')
+pop_gdp = pop_gdp[['Year', 'Country Name', 'Population', 'Gdp']]
 
+pop_gdp = pop_gdp[pop_gdp['Country Name'].isin(np.intersect1d(em.Country.unique(), pop_gdp['Country Name'].unique()))]
+em = em[em.Country.isin(np.intersect1d(em.Country.unique(), pop_gdp['Country Name'].unique()))]
+em.rename(columns={'Country': 'Country Name', 'Total': 'Total emissions', 'Per Capita': 'Emissions per Capita'},
+          inplace='True')
+pop_gdp.Year = pop_gdp.Year.astype('int64')
+data = pd.merge(pop_gdp,em, how='left', on=['Year','Country Name'])
