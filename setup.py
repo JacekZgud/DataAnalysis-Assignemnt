119ac43a810f0b46d3479326fca4098219678d0a
Jacek <jacek.zgud2@gmail.com>
2023-02-07 17:05:24 +0100

    package issues fix v4

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
-with open("./Final_package/README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
